39b681e040e1be14519e9418cb6bef6f159f4150
Neeraj Doharey <neerajlivewire@gmail.com>
2018-02-15 22:17:25 +0530

    Changes in custom pagination

--- config/initializers/custom_pagination.rb
+++ config/initializers/custom_pagination.rb
@@ -1,29 +1,29 @@
-class MyCustomLinkRenderer < WillPaginate::ActionView::LinkRenderer 
-    def html_container(html)
-      tag(:nav, tag(:ul, html, class: "pagination text-center"))
-    end
+class MyCustomLinkRenderer < WillPaginate::ActionView::LinkRenderer
+  def html_container(html)
+    tag(:nav, tag(:ul, html, class: "pagination text-center"))
+  end
 
-    def previous_or_next_page(page, text, classname)
-      if page
-        tag(:li, link(text, page, class: "pagination-link"))
+  def previous_or_next_page(page, text, classname)
+    if page
+      tag(:li, link(text, page, class: "pagination-link"))
+    else
+      if text == "← Previous"
+        tag(:li, text , class: "pagination-previous disabled")
       else
-        if text == "← Previous"
-          tag(:li, text , class: "pagination-previous disabled")
-        else
-          tag(:li, text, class: "pagination-next disabled")
-        end
+        tag(:li, text, class: "pagination-next disabled")
       end
     end
+  end
 
-    def page_number(page)
-      unless page == current_page
-        tag(:li, link(page, page, class: "pagination-link"))
-      else
-        tag(:li, tag(:a, page, class: "pagination-link current"))
-      end
+  def page_number(page)
+    unless page == current_page
+      tag(:li, link(page, page, class: "pagination-link"))
+    else
+      tag(:li, tag(:a, page, class: "pagination-link current"))
     end
+  end
 
-    def gap
-      tag(:li, tag(:span, '&hellip;', class: "ellipsis"))
+  def gap
+    tag(:li, tag(:span, '&hellip;', class: "ellipsis"))
     end
 end
