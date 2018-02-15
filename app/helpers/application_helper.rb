6d60ce1d6cc17b3ef4a5aeff2b19463e72f8aa30
Neeraj Doharey <neerajlivewire@gmail.com>
2018-02-15 21:54:02 +0530

    Add Customized pagination

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,13 +8,13 @@ module ApplicationHelper
     link_to(name, '#', class: "add_fields " + args[:class], data: {id: id, fields: fields.gsub("\n", "")})
   end
 
-  # def will_paginate(collection_or_options = nil, options = {})
-  #   if collection_or_options.is_a? Hash
-  #     options, collection_or_options = collection_or_options, nil
-  #   end
-  #   unless options[:renderer]
-  #     options = options.merge :renderer => MyCustomLinkRenderer
-  #   end
-  #   super *[collection_or_options, options].compact
-  # end
+  def will_paginate(collection_or_options = nil, options = {})
+    if collection_or_options.is_a? Hash
+      options, collection_or_options = collection_or_options, nil
+    end
+    unless options[:renderer]
+      options = options.merge :renderer => MyCustomLinkRenderer
+    end
+    super *[collection_or_options, options].compact
+  end
 end
--- config/initializers/custom_pagination.rb
+++ config/initializers/custom_pagination.rb
@@ -1,29 +1,29 @@
-# class MyCustomLinkRenderer < WillPaginate::ActionView::LinkRenderer 
-#     def html_container(html)
-#       tag(:nav, tag(:ul, html, class: "pagination text-center"))
-#     end
+class MyCustomLinkRenderer < WillPaginate::ActionView::LinkRenderer 
+    def html_container(html)
+      tag(:nav, tag(:ul, html, class: "pagination text-center"))
+    end
 
-#     def previous_or_next_page(page, text, classname)
-#       if page
-#         tag(:li, link(text, page, class: "pagination-link"))
-#       else
-#         if text == "← Previous"
-#           tag(:li, text , class: "pagination-previous disabled")
-#         else
-#           tag(:li, text, class: "pagination-next disabled")
-#         end
-#       end
-#     end
+    def previous_or_next_page(page, text, classname)
+      if page
+        tag(:li, link(text, page, class: "pagination-link"))
+      else
+        if text == "← Previous"
+          tag(:li, text , class: "pagination-previous disabled")
+        else
+          tag(:li, text, class: "pagination-next disabled")
+        end
+      end
+    end
 
-#     def page_number(page)
-#       unless page == current_page
-#         tag(:li, link(page, page, class: "pagination-link"))
-#       else
-#         tag(:li, tag(:a, page, class: "pagination-link current"))
-#       end
-#     end
+    def page_number(page)
+      unless page == current_page
+        tag(:li, link(page, page, class: "pagination-link"))
+      else
+        tag(:li, tag(:a, page, class: "pagination-link current"))
+      end
+    end
 
-#     def gap
-#       tag(:li, tag(:span, '&hellip;', class: "ellipsis"))
-#     end
-# end
+    def gap
+      tag(:li, tag(:span, '&hellip;', class: "ellipsis"))
+    end
+end
